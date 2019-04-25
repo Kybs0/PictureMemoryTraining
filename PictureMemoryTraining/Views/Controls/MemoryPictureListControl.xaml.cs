9446e4496d0639109d84f15b70733f5ceb4ddef4
kybs0 <1016724958@qq.com>
2019-04-26 00:15:21 +0800

    确认按钮

--- PictureMemoryTraining/Views/Controls/MemoryPictureListControl.xaml.cs
+++ PictureMemoryTraining/Views/Controls/MemoryPictureListControl.xaml.cs
@@ -130,31 +130,35 @@ namespace PictureMemoryTraining.Views
         }
 
         public event EventHandler<LocationMemoryPictureItem> PictureLocationComfirmed;
-        private void YesButton_OnClick(object sender, RoutedEventArgs e)
+        private async void YesButton_OnClick(object sender, RoutedEventArgs e)
         {
-            var memoryPictureItem = MemoryPictureItems.First(i => i.IsPictureVisibile);
-            memoryPictureItem.IsPictureVisibile = false;
-            memoryPictureItem.IsPictureCovered = true;
-            PictureLocationComfirmed?.Invoke(this, new LocationMemoryPictureItem()
-            {
-                PictureItem = memoryPictureItem,
-                Location = MemoryPictureItems.IndexOf(memoryPictureItem),
-                IsMatchedByUserComfirmed = true
-            });
-
+            await ComfirmLocation(sender, true);
         }
 
-        private void NoButton_OnClick(object sender, RoutedEventArgs e)
+        private async Task ComfirmLocation(object sender, bool comfirmed)
         {
-            var memoryPictureItem = MemoryPictureItems.First(i => i.IsPictureVisibile);
-            memoryPictureItem.IsPictureVisibile = false;
-            memoryPictureItem.IsPictureCovered = true;
-            PictureLocationComfirmed?.Invoke(this, new LocationMemoryPictureItem()
+            this.IsEnabled = false;
+            if (sender is Button button)
             {
-                PictureItem = memoryPictureItem,
-                Location = MemoryPictureItems.IndexOf(memoryPictureItem),
-                IsMatchedByUserComfirmed = false
-            });
+                button.IsEnabled = false;
+                await Task.Delay(TimeSpan.FromMilliseconds(600));
+                var memoryPictureItem = MemoryPictureItems.First(i => i.IsPictureVisibile);
+                memoryPictureItem.IsPictureVisibile = false;
+                memoryPictureItem.IsPictureCovered = true;
+                PictureLocationComfirmed?.Invoke(this, new LocationMemoryPictureItem()
+                {
+                    PictureItem = memoryPictureItem,
+                    Location = MemoryPictureItems.IndexOf(memoryPictureItem),
+                    IsMatchedByUserComfirmed = comfirmed
+                });
+                button.IsEnabled = true;
+            }
+            this.IsEnabled = true;
+        }
+
+        private async void NoButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            await ComfirmLocation(sender, false);
         }
 
         #endregion
@@ -198,7 +202,7 @@ namespace PictureMemoryTraining.Views
                 if (_selectedSequentialPictureList.Any(i => i == memoryPictureItem))
                 {
                     _selectedSequentialPictureList.Remove(memoryPictureItem);
-                    var testingClickInfo = _testRecordInfo.SequentialTestingClickInfos.First(i=>i.PictureName==Path.GetFileNameWithoutExtension(memoryPictureItem.ImageUri));
+                    var testingClickInfo = _testRecordInfo.SequentialTestingClickInfos.First(i => i.PictureName == Path.GetFileNameWithoutExtension(memoryPictureItem.ImageUri));
                     _testRecordInfo.SequentialTestingClickInfos.Remove(testingClickInfo);
                 }
             }
